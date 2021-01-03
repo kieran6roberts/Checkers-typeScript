d4bdaf1fae90132a526c00048d33acdff8e798ac
unknown <kieran6roberts@gmail.com>
2021-01-03 19:17:09 +0100

    basic webpack setup

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,6 +2,7 @@ import appendScoreToDOM from "./appendScoreToDOM";
 import initPlayerPieces from "./initPlayerPieces";
 import setCurrentPieceHandler from "./setCurrentPieceHandler";
 import { gameControl } from "./control";
+import "../../main.css";
 
 const gameInit = (): void => {
   initPlayerPieces(gameControl.currentPlayer, setCurrentPieceHandler);
---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/ts/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ]
+  },
+  resolve: {
+    extensions: [ ".tsx", ".ts", ".js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "TypeScript Checkers",
+      filename: "index.html",
+      template: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.css"
+    })
+  ]
+};
